9fb58f151dbf0e7cad92ded855816c0a25079816
Bosko Kulusic <kulusicbosko@gmail.com>
2024-03-28 20:13:08 +0100

    feat: return bad request, not found and ok status codes

--- ProjekatSoapRest/ProjekatSoapRest/Service/Concrete/CompanyService.svc.cs
+++ ProjekatSoapRest/ProjekatSoapRest/Service/Concrete/CompanyService.svc.cs
@@ -2,13 +2,14 @@
 using System;
 using System.Collections.Generic;
 using System.IO;
+using System.ServiceModel.Web;
 using System.Xml.Serialization;
 
 namespace ProjekatSoapRest
 {
     public class CompanyService : ICompanyServiceRest, ICompanyServiceSoap, IValidator
     {
-        public List<Company> GetCompanies()
+        private List<Company> getCompanies()
         {
             var mySerializer = new XmlSerializer(typeof(List<Company>));
             var myFileStream = new FileStream(AppDomain.CurrentDomain.BaseDirectory + "/Data/companies.xml", FileMode.Open);
@@ -19,23 +20,36 @@ namespace ProjekatSoapRest
 
         private Company addCompany(Company company)
         {
+            WebOperationContext ctx = WebOperationContext.Current;
             if (!Validate(company))
             {
+                ctx.OutgoingResponse.StatusCode = System.Net.HttpStatusCode.BadRequest;
                 return null;
             }
-            List<Company> companies = GetCompanies();
+            List<Company> companies = getCompanies();
             companies.Add(company);
             XmlSerializer x = new XmlSerializer(companies.GetType());
             StreamWriter myWriter = new StreamWriter(AppDomain.CurrentDomain.BaseDirectory + "/Data/companies.xml");
             x.Serialize(myWriter, companies);
             myWriter.Close();
+            ctx.OutgoingResponse.StatusCode = System.Net.HttpStatusCode.OK;
             return company;
         }
 
         private Company getCompanyById(string companyId)
         {
-            var companies = GetCompanies();
-            return companies.Find(c => c.Id.ToString().Equals(companyId));
+            var company = getCompanies().Find(c => c.Id.ToString().Equals(companyId));
+            WebOperationContext ctx = WebOperationContext.Current;
+            if(company != null)
+            {
+                ctx.OutgoingResponse.StatusCode = System.Net.HttpStatusCode.OK;
+            }
+            else
+            {
+                ctx.OutgoingResponse.StatusCode = System.Net.HttpStatusCode.NotFound;
+            }
+            return company;
+
         }
         public Company AddCompanySoap(Company company)
         {
@@ -59,7 +73,7 @@ namespace ProjekatSoapRest
 
         public bool Validate(Company company)
         {
-            List<Company> companies = GetCompanies();
+            List<Company> companies = getCompanies();
             if (companies.Find(c => c.Id.ToString().Equals(company.Id)) != null)
             {
                 return false;

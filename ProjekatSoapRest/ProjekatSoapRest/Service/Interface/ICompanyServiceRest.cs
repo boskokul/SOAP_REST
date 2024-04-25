989fb9107f76cf69869c616952c4da0c999a9cdf
Bosko Kulusic <kulusicbosko@gmail.com>
2024-04-25 12:39:57 +0200

    rest json example

--- ProjekatSoapRest/ProjekatSoapRest/Service/Interface/ICompanyServiceRest.cs
+++ ProjekatSoapRest/ProjekatSoapRest/Service/Interface/ICompanyServiceRest.cs
@@ -8,7 +8,7 @@ namespace ProjekatSoapRest
     public interface ICompanyServiceRest
     {
         // post http://localhost:52336/Concrete/CompanyService.svc/rest/Company
-        // primer jsona za ovaj post: {"Id":"5","Name":"PepsiCo", "Departments":["Tehnoloski", "Prodajni"], "Employees":[{"JMBG":"12", "FirstName":"Marko", "LastName":"Markovic", "Email":"markoni@gmail.com", "DeservesRaise":false, "DateOfBirth":"2017-09-08 11:20:12"}]}
+        // primer jsona za ovaj post: {"Id":"187","Name":"PepsiCo", "Departments":[{"Name":"Tehnoloski"}, {"Name":"Prodajni"}], "Employees":[{"JMBG":"12", "FirstName":"Marko", "LastName":"Markovic", "Email":"markoni@gmail.com", "DeservesRaise":false, "DateOfBirth":"2017-09-08 11:20:12"}]}
         [OperationContract]
         [WebInvoke(Method = "POST",
             UriTemplate = "/Company",

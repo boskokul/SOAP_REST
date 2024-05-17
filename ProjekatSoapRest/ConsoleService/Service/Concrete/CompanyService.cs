b8583a62a946a5569abf18e2bd99c88809b504a2
Bosko Kulusic <kulusicbosko@gmail.com>
2024-05-17 20:46:59 +0200

    some code cleanup

--- ProjekatSoapRest/ConsoleService/Service/Concrete/CompanyService.cs
+++ ProjekatSoapRest/ConsoleService/Service/Concrete/CompanyService.cs
@@ -1,9 +1,6 @@
-﻿using System;
-using System.Collections.Generic;
-using System.IO;
+﻿using System.Collections.Generic;
 using System.Linq;
 using System.Net;
-using System.Xml.Serialization;
 using ProjekatSoapRest.Model;
 using ConsoleService.Service.Interface;
 using ConsoleService.Data;
